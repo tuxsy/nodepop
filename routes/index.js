ad0607efc8027a30a2bb784ca051ebef362ad224
Bruno Anglés Robles <bangles@gmail.com>
2017-12-12 00:56:31 +0100

    Añadido i18n

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var i18n = require('i18n');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const lang = req.query.lang || 'en';
+  console.log('lang', lang);
+  i18n.setLocale(req, lang);
+  res.locals.title = res.__('Express');
+  res.render('index');
 });
 
 module.exports = router;
